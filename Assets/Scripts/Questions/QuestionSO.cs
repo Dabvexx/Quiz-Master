0b3ff82aea0caeb4151ea3faa543979ef21ea5df
Dabvexx <alexanderwoods2019@gmail.com>
2022-03-24 13:03:29 -0400

    Arrays

--- Assets/Scripts/Questions/QuestionSO.cs
+++ Assets/Scripts/Questions/QuestionSO.cs
@@ -9,12 +9,27 @@ public class QuestionSO : ScriptableObject
     // Variables.
     [TextArea(2,6)]
     [SerializeField] string question = "Enter question text here";
+
+    // Personally, i would make the questions able to be randomized where they are displayed,
+    // and have the first answer in the array to be the correct answer.
+    [SerializeField] string[] answers = new string[4];
+    [SerializeField] int correctAnswerIndex;
     #endregion
 
     #region Public Methods
     public string GetQuestion()
     {
         return question;
+    }
+
+    public string GetAnswer(int index)
+    {
+        return answers[index];
+    }
+
+    public int getCorrectAnswerIndex()
+    {
+        return correctAnswerIndex;
     }
     #endregion 
 }
